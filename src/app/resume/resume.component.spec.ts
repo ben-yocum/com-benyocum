7c9d21b9a8dae279e97cfffd59c4b1ff6562d6ed
ben-yocum <waerstein@gmail.com>
2026-08-17 10:03:22 -0700

    Change source of resume to a local PDF file.

--- src/app/resume/resume.component.spec.ts
+++ src/app/resume/resume.component.spec.ts
@@ -21,4 +21,10 @@ describe('ResumeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have the correct iframe source', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const iframe = compiled.querySelector('iframe');
+    expect(iframe?.src).toContain('assets/Benjamin%20Yocum%20Resume%202026%20Revised.pdf');
+  });
 });
